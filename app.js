26a4913a8031105cd649d6b0f3e71507dc8d7115
michelmfreitas <michelmfreitas@users.noreply.github.com>
2020-04-13 12:57:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+  const project = { id: uuid(), title, url, techs, likes: 0 } ;
+  repositories.push(project);
+  return response.json(project);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const projectIndex = repositories.findIndex(project => project.id === id);
+  if(projectIndex < 0){
+    return response.status(400).json({error: "Project not found"});
+  }
+
+  const likes = repositories[projectIndex].likes;
+
+  const project = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const projectIndex = repositories.findIndex(project => project.id === id);
+  if(projectIndex < 0){
+    return response.status(400).json({error: "Project not found"});
+  }
+
+  repositories.splice(projectIndex, 1);
+
+  return response.status(204).send();
+
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+  const projectIndex = repositories.findIndex(project => project.id === id);
+  if(projectIndex < 0){
+    return response.status(400).json({error: "Project not found"});
+  }
+  const likes = repositories[projectIndex].likes + 1;
+  const {title, techs, url } = repositories[projectIndex]
+  const project = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories[projectIndex] = project;
+  return response.json(project);
+});
+
+module.exports = app;
